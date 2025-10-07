6a148d16046aa4690377cd0c52c87e019589d4a0
JAyenGreen <jeff.greenberg@theaccidentalcoder.com>
2025-10-06 21:57:02 -0400

    Adding ability to specify image style in twig call.

--- web/modules/custom/unmanaged_files/src/Twig/UnmanagedFilesExtension.php
+++ web/modules/custom/unmanaged_files/src/Twig/UnmanagedFilesExtension.php
@@ -1,52 +1,35 @@
-<?php
-
-namespace Drupal\unmanaged_files\Twig;
-
-use Drupal\unmanaged_files\Service\FileHandler;
-use Drupal\Core\File\FileUrlGeneratorInterface;
-use Twig\Extension\AbstractExtension;
-use Twig\TwigFunction;
+use Drupal\image\Entity\ImageStyle;
+// Add at top of file if not present.
 
 /**
- * Twig extension exposing unmanaged file helpers to templates.
+ * Returns either a file URL or an <img> tag for a random unmanaged file.
+ *
+ * @param string $format
+ *   'url' (default) to return the file URL, or 'img' to return an <img> tag.
+ * @param string|null $style
+ *   (optional) The image style name to apply when $format is 'img'.
+ *
+ * @return string|null
+ *   URL or HTML string, or NULL if no files found.
  */
-final class UnmanagedFilesExtension extends AbstractExtension {
-
-  public function __construct(
-    private FileHandler $handler,
-    private FileUrlGeneratorInterface $urlGen,
-  ) {}
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getFunctions(): array {
-    return [
-      new TwigFunction('random_unmanaged_file', [$this, 'getRandomFile'], ['is_safe' => ['html']]),
-    ];
+public function getRandomFile(string $format = 'url', ?string $style = NULL): ?string {
+  $uri = $this->handler->getRandomFile();
+  if (!$uri) {
+    return NULL;
   }
 
-  /**
-   * Returns either a file URL or an <img> tag for a random unmanaged file.
-   *
-   * @param string $format
-   *   'url' (default) to return the file URL, or 'img' to return an <img> tag.
-   *
-   * @return string|null
-   *   URL or HTML string, or NULL if no files found.
-   */
-  public function getRandomFile(string $format = 'url'): ?string {
-    $uri = $this->handler->getRandomFile();
-    if (!$uri) {
-      return NULL;
+  // Get the base file URL.
+  $url = $this->urlGen->generateAbsoluteString($uri);
+
+  // Apply image style if requested.
+  if ($format === 'img' && $style) {
+    if ($image_style = ImageStyle::load($style)) {
+      $url = $image_style->buildUrl($uri);
     }
-
-    $url = $this->urlGen->generateAbsoluteString($uri);
-
-    return match ($format) {
-      'img' => '<img src="' . $url . '" alt="Random unmanaged file">',
-      default => $url,
-    };
   }
 
+  return match ($format) {
+    'img' => '<img src="' . $url . '" alt="Random unmanaged file">',
+    default => $url,
+  };
 }
